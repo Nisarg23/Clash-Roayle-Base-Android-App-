6bfdad3fb25dfac17c48601f7860a9dc6b8a92f6
Nisarg Patel <npate112@uottawa.ca>
2020-06-17 17:39:50 -0400

    s

--- app/src/main/java/com/example/clashroyalebase/PlayerInfo.java
+++ app/src/main/java/com/example/clashroyalebase/PlayerInfo.java
@@ -6,30 +6,61 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.example.clashroyalebase.ui.slideshow.SlideshowFragment;
+
 public class PlayerInfo extends AppCompatActivity {
 
-    public static ImageView[] chest_buttons;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_info);
+    ImageView chest1 = findViewById(R.id.chest1);
+      ImageView chest2 = findViewById(R.id.chest2);
+      ImageView chest3 = findViewById(R.id.chest3);
+      ImageView chest4 = findViewById(R.id.chest4);
+      ImageView chest5 = findViewById(R.id.chest5);
+      ImageView chest6 = findViewById(R.id.chest6);
+      ImageView chest7 = findViewById(R.id.chest7);
+     ImageView chest8 = findViewById(R.id.chest8);
+     ImageView chest9 = findViewById(R.id.chest9);
+     ImageView chest10 = findViewById(R.id.chest10);
+     ImageView chest11 = findViewById(R.id.chest11);
+     ImageView chest12 = findViewById(R.id.chest12);
+     ImageView chest13 = findViewById(R.id.chest13);
+     ImageView chest14 = findViewById(R.id.chest14);
 
-        ImageView chest1 = findViewById(R.id.chest1);
-        ImageView chest2 = findViewById(R.id.chest2);
-        ImageView chest3 = findViewById(R.id.chest3);
-        ImageView chest4 = findViewById(R.id.chest4);
-        ImageView chest5 = findViewById(R.id.chest5);
-        ImageView chest6 = findViewById(R.id.chest6);
-        ImageView chest7 = findViewById(R.id.chest7);
-        ImageView chest8 = findViewById(R.id.chest8);
-        ImageView chest9 = findViewById(R.id.chest9);
-        ImageView chest10 = findViewById(R.id.chest10);
-        ImageView chest11 = findViewById(R.id.chest11);
-        ImageView chest12 = findViewById(R.id.chest12);
-        ImageView chest13 = findViewById(R.id.chest13);
-        ImageView chest14 = findViewById(R.id.chest14);
+     ImageView[] chest_buttons =new ImageView[]{
+        chest1, chest2, chest3, chest4,
+                chest5, chest6, chest7, chest8, chest9, chest10, chest11, chest12, chest13, chest14
+    };
 
-        chest_buttons = new ImageView[]{chest1,chest2,chest3,chest4,
-        chest5,chest6,chest7,chest8,chest9,chest10,chest11,chest12,chest13,chest14};
+    for (int i=0; i<14;i++){
+        switch (SlideshowFragment.chest.get(i)){
+            case "Silver Chest":
+                chest_buttons[i].setImageResource(R.drawable.silver_chest);
+                break;
+            case "Giant Chest":
+                chest_buttons[i].setImageResource(R.drawable.giant_chest);
+                break;
+            case "Magical Chest":
+                chest_buttons[i].setImageResource(R.drawable.magical_chest);
+                break;
+            case "Legendary Chest":
+                chest_buttons[i].setImageResource(R.drawable.legendary_chest);
+                break;
+            case "Golden Chest":
+                chest_buttons[i].setImageResource(R.drawable.gold_chest);
+                break;
+            case "Epic Chest":
+                chest_buttons[i].setImageResource(R.drawable.epic_chest);
+                break;
+            case "Mega Lightning Chest":
+                chest_buttons[i].setImageResource(R.drawable.mega_lightning_chest);
+                break;
+        }
+    }
+
     }
 }
--- app/src/main/java/com/example/clashroyalebase/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/clashroyalebase/ui/slideshow/SlideshowFragment.java
@@ -2,6 +2,7 @@ package com.example.clashroyalebase.ui.slideshow;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -28,6 +30,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class SlideshowFragment extends Fragment {
 
@@ -38,11 +41,15 @@ public class SlideshowFragment extends Fragment {
 
     static Boolean invalid;
 
+
     JSONObject obj;
 
     public static String base_url = "https://api.clashroyale.com/v1/players/";
     public static String url_player;
     public static String url_chest;
+
+    public static ArrayList<String> chest = new ArrayList<String>();
+    public static ArrayList<Integer> chestAt = new ArrayList<Integer>();
 
     public final String key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjVmYjEyNDI0LWIwODEtNDMwNC05MWQ0LTlkNjIyYmEwN2M5OSIsImlhdCI6MTU5MjIzNDk3MSwic3ViIjoiZGV2ZWxvcGVyLzBlZjM4ZDg5LWI1MjItYThhMC01YWE2LTQ1OGZlNjZmMTM4ZSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyNC4yMzUuMjEwLjc2Il0sInR5cGUiOiJjbGllbnQifV19.aCOBc9Gg2fyKCcd4GiyO3kKvG5tPWp52vWWLdJ_L8e_8WB9WVAorbx3fTiej3dfIbIK7rCKveNVwTUdWCLBJ3w";
 
@@ -55,9 +62,10 @@ public class SlideshowFragment extends Fragment {
         player_tag = root.findViewById(R.id.player_tag);
         search_player = root.findViewById(R.id.search_player);
 
+
+
+
         searchButtonListener();
-
-
 
 
         return root;
@@ -70,14 +78,7 @@ public class SlideshowFragment extends Fragment {
                 final String tag = "%23"+player_tag.getText().toString().toUpperCase();
                 url_player = base_url + tag;
                 url_chest = url_player.concat("/upcomingchests");
-
-                System.out.println(url_chest.charAt(51));
-                System.out.println(url_chest.charAt(50));
-                System.out.println(url_chest.charAt(49));
-
-
-
-                System.out.println(url_chest);
+                url_chest = url_chest.replaceAll("\\s+","");
 
                 InputMethodManager mgr = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
                 mgr.hideSoftInputFromWindow(view.getWindowToken(), 0);
@@ -109,10 +110,6 @@ public class SlideshowFragment extends Fragment {
                             in.close();
                              obj = new JSONObject(response.toString());
 
-                            // print result
-                            for (int i=0; i<obj.length();i++){
-                                System.out.println(obj.keys());
-                            }
 
 
 
@@ -145,8 +142,9 @@ public class SlideshowFragment extends Fragment {
                                     try {
                                         JSONArray array = (JSONArray) obj.get("items");
                                         for (int i = 0; i < array.length(); i++) {
-                                            //System.out.println(array.get(i));
                                             JSONObject o = new JSONObject(array.get(i).toString());
+                                            chest.add((String) o.get("name"));
+                                            chestAt.add((Integer) o.get("index"));
                                             System.out.println(o.get("name") + "   " + o.get("index"));
                                         }
                                     }catch(JSONException e){
@@ -157,6 +155,7 @@ public class SlideshowFragment extends Fragment {
 
 
                                     startActivity(new Intent(getActivity(), PlayerInfo.class));
+
                                 }
                             }
                         });
