9a8c8eee53e8ea35f9610e5e00a9e664c4568263
Nisarg Patel <npate112@uottawa.ca>
2020-07-13 13:36:05 -0400

    s

--- app/src/main/java/com/example/clashroyalebase/TrialActivity.java
+++ app/src/main/java/com/example/clashroyalebase/TrialActivity.java
@@ -6,12 +6,19 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class TrialActivity extends AppCompatActivity {
 
@@ -20,6 +27,36 @@ public class TrialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trial);
+
+        ImageView s = findViewById(R.id.aoc);
+
+
+        s.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Uri app = Uri.parse("clashroyale://copyDeck?deck=26000010;26000014;26000021;26000030;26000038;27000000;28000000;28000011");
+                //Intent intent = getPackageManager().getLaunchIntentForPackage("com.supercell.clashroyale");
+                Intent intent = new Intent(Intent.ACTION_VIEW,app);
+
+                PackageManager packageManager = getPackageManager();
+                List<ResolveInfo> activities = packageManager.queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+                boolean isIntentSafe = activities.size() > 0;
+
+                if (isIntentSafe == true) {
+                    startActivity(intent);
+                }
+                else{
+                    Toast t = Toast.makeText(TrialActivity.this,
+                            "You Don't Have Clash Royale",
+                            Toast.LENGTH_SHORT);
+                    t.show();
+
+                }
+            }
+        });
+
 
     }
 
